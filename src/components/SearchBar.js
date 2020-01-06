647558726b2d721ff4efbe03ecf2b7c255bdd2fd
DeeKat <deekat.devverse@gmail.com>
2020-01-07 00:36:35 +0530

    adding event handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
-                        <label for="search">Image Search</label>
-                        <input id="search" type="text" />
+                        <label htmlFor="search">Image Search</label>
+                        <input id="search" type="text" onChange={this.onInputChange} />
+                        {/* <input id="search" type="text" onChange={ (e) => console.log(e.target.value) } /> */}
                     </div>
                 </form>
             </div>
